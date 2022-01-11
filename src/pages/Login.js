3a1ddaec95226ef8064dbc585880f12043ad8063
Rael N. Pires <rnogpiresfinancial@gmail.com>
2022-01-11 15:34:42 -0300

    email and password validation implemented

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,26 +1,67 @@
 import React from 'react';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      btnDisabled: true,
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.validation = this.validation.bind(this);
+  }
+
+  validation() {
+    const { email, password } = this.state;
+    const regex = /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/g;
+    const MIN_PASSWORD_LENGTH = 6;
+
+    if (password !== undefined) {
+      if (password.length >= MIN_PASSWORD_LENGTH && regex.test(email)) {
+        this.setState({ btnDisabled: false });
+      } else {
+        this.setState({ btnDisabled: true });
+      }
+    }
+  }
+
+  handleChange({ target }) {
+    const { id, value } = target;
+    // https://pt-br.reactjs.org/docs/react-component.html#setstate
+    // callback as second parameter in setState method
+    this.setState({ [id]: value }, () => this.validation());
+  }
+
   render() {
+    const { btnDisabled } = this.state;
+
     return (
       <div id="login">
-        <label htmlFor="email-input">
+        <label htmlFor="email">
           <input
-            type="text"
+            type="email"
             placeholder="Email"
-            id="email-input"
+            id="email"
             data-testid="email-input"
+            onChange={ this.handleChange }
           />
         </label>
-        <label htmlFor="password-input">
+        <label htmlFor="password">
           <input
             type="text"
             placeholder="Password"
-            id="password-input"
+            id="password"
             data-testid="password-input"
+            onChange={ this.handleChange }
           />
         </label>
-        <button type="button">Entrar</button>
+        <button
+          type="button"
+          disabled={ btnDisabled }
+        >
+          Entrar
+        </button>
       </div>
     );
   }
