f66e0c225d1593dfa11badd8d3334470c70da5eb
Rael N. Pires <rnogpiresfinancial@gmail.com>
2022-01-11 14:40:35 -0300

    Route path implemented on Login component

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,16 @@ import React from 'react';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <div id="login">
+        <label htmlFor="email-input">
+          <input type="text" placeholder="Email" id="email-input" />
+        </label>
+        <label htmlFor="password-input">
+          <input type="text" placeholder="Password" id="password-input" />
+        </label>
+      </div>
+    );
   }
 }
 
