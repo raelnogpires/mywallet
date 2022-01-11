90dc3a5d00bec8d736d4572465108f54716e8579
Rael N. Pires <rnogpiresfinancial@gmail.com>
2022-01-11 14:48:17 -0300

    email input, password input and button implemented

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -5,11 +5,22 @@ class Login extends React.Component {
     return (
       <div id="login">
         <label htmlFor="email-input">
-          <input type="text" placeholder="Email" id="email-input" />
+          <input
+            type="text"
+            placeholder="Email"
+            id="email-input"
+            data-testid="email-input"
+          />
         </label>
         <label htmlFor="password-input">
-          <input type="text" placeholder="Password" id="password-input" />
+          <input
+            type="text"
+            placeholder="Password"
+            id="password-input"
+            data-testid="password-input"
+          />
         </label>
+        <button type="button">Entrar</button>
       </div>
     );
   }
